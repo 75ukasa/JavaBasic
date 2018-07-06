f8ebe233da7c816454e1064eb724e8ac551ae087
DESKTOP-92NNIOD\let-i <let.it.be180620@gmail.com>
2018-07-06 18:35:12 +0900

    11-3

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -9,12 +9,26 @@ package practice11;
 public class PTra11_02 {
 	public static void main(String[] args) {
 
-		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
-
-
+		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します]
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
+		String title  = scanner.nextLine();
+
+	
+
+		Book[] BookData = FileReaderClass.readBookDataFile();
+		for(int i = 0 ; i < BookData.length ; i++ ){
+			if(BookData[i].title.contains(title)) {
+				System.out.println(BookData[i].dispBookInfo());
+			}
+		}
+
+
+			
+
+
 
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
-
+		
 	}
 }
